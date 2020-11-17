fddc7f2a76248ebc1c881b9c0501c9aa08ae6243
Pierre Cavalet <pcavalet@kaliop.com>
2020-11-17 11:18:17 +0100

    TextField - vue component

---
+++ components/TextField/TextField.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input
+      class="TextField"
+      type="text"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    label: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@components/TextField/_textField.scss';
+</style>
