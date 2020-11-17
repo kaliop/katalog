ff84c2f046880d676daa2c2ce1ccba1194b44c12
Pierre Cavalet <pcavalet@kaliop.com>
2020-11-17 12:15:59 +0100

    Radio - vue component

---
+++ components/Radio/Radio.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label class="Radio" :class="{ 'Radio-selected': checked }">
+      <span v-if="checked">x</span>
+      <span v-else>o</span>
+      <span>{{ label }}</span>
+      <input
+        type="radio"
+        :name="name"
+        :value="value"
+        :checked="checked"
+        @change="$emit('change', $event.target.value)"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Radio',
+  model: {
+    prop: 'modelValue',
+    event: 'change'
+  },
+  props: {
+    name: String,
+    value: String,
+    label: String,
+    modelValue: String
+  },
+  computed: {
+    checked() {
+      return this.modelValue === this.value
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@components/Radio/_radio.scss';
+</style>
